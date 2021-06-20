fa4b6c4ed1452b7e1be321f92f3de991b311220a
Baki OZTURK <22.exe.94@gmail.com>
2021-06-21 00:12:34 +0300

    GroupExcept Groups ve OthersInGroup methodları kullanmak

--- SignalRApplication/Hubs/MessageHub.cs
+++ SignalRApplication/Hubs/MessageHub.cs
@@ -12,51 +12,73 @@ namespace SignalRApplication.Hubs
         //Aynı zamanda bu method clienttaki methodu tetikler.
         //public async Task SendMessageAsync(string message, IEnumerable<string> connectionIds)
         //{
-            #region CLIENT TURLERI
+        #region CLIENT TURLERI
 
-            #region All
-            // Tüm clientlara gönderir.
-            //await Clients.All.SendAsync("receiveMessageOnClient", message);
-            #endregion
-            #region Caller
-            // Sadece servera bildirim gönderen clientla etkileşime geçer.
-            //await Clients.Caller.SendAsync("receiveMessageOnClient", message);
-            #endregion
-            #region Other
-            //  Bildirim gönderen client dışındaki tüm bağlı olan clientlara bildirim gönderir.
-            //await Clients.Other.SendAsync("receiveMessageOnClient", message);
-            #endregion
+        #region All
+        // Tüm clientlara gönderir.
+        //await Clients.All.SendAsync("receiveMessageOnClient", message);
+        #endregion
+        #region Caller
+        // Sadece servera bildirim gönderen clientla etkileşime geçer.
+        //await Clients.Caller.SendAsync("receiveMessageOnClient", message);
+        #endregion
+        #region Other
+        //  Bildirim gönderen client dışındaki tüm bağlı olan clientlara bildirim gönderir.
+        //await Clients.Other.SendAsync("receiveMessageOnClient", message);
+        #endregion
 
-            #endregion
+        #endregion
 
-            #region HUB CLIENT METHODLARI
+        #region HUB CLIENT METHODLARI
 
-            #region AllExcept
-            //Belirtilen clientlar hariç tüm clientlara bildirimde bulunur.
+        #region AllExcept
+        //Belirtilen clientlar hariç tüm clientlara bildirimde bulunur.
 
-            //await Clients.AllExcept(connectionIds).SendAsync("receiveMessageOnClient", message);
-            #endregion
-            #region Client
-            //Sadece ConnectionId ile Belirtilen clienta bildirim gönderir.
-            //Whatsapptaki bir kişiye mesaj atmak bununla yapılır.
+        //await Clients.AllExcept(connectionIds).SendAsync("receiveMessageOnClient", message);
+        #endregion
+        #region Client**
+        //Sadece ConnectionId ile Belirtilen clienta bildirim gönderir.
+        //Whatsapptaki bir kişiye mesaj atmak bununla yapılır.
 
-            //await Clients.Client(connectionIds.FirstOrDefault()).SendAsync("receiveMessageOnClient", message);
-            #endregion
-            #region Clients
-            //Sadece ConnectionId ile Belirtilen clientlara bildirim gönderir.
-            //Whatsapptaki toplu mesaj atmak bununla yapılır.
+        //await Clients.Client(connectionIds.FirstOrDefault()).SendAsync("receiveMessageOnClient", message);
+        #endregion
+        #region Clients
+        //Sadece ConnectionId ile Belirtilen clientlara bildirim gönderir.
+        //Whatsapptaki toplu mesaj atmak bununla yapılır.
 
-            //await Clients.Clients(connectionIds).SendAsync("receiveMessageOnClient", message);
-            #endregion
-            #region Group
-            //Belirtilen gruptaki tüm clientlara bildirimde bulunur.
-            //Tabiki ilgili grupu oluşturmamız gerekıyor (addGroup methodunu olusturduk)
-            //öncesinde ilgili clienta group sectırdık. ve butona bastıgında addGroup fonk calıstı.Ardından alttaki method Group.SendAsync
-            //methodu ile grouptaki clientlara mesaj gonderdık.
-            //Group name lazım old. için. 
-            public async Task SendMessageAsync(string message, string groupName)
-            {
-                await Clients.Group(groupName).SendAsync("receiveMessageOnClient", message);
+        //await Clients.Clients(connectionIds).SendAsync("receiveMessageOnClient", message);
+        #endregion
+        #region Group**
+        //Belirtilen gruptaki tüm clientlara bildirimde bulunur.
+        //Tabiki ilgili grupu oluşturmamız gerekıyor (addGroup methodunu olusturduk)
+        //öncesinde ilgili clienta group sectırdık. ve butona bastıgında addGroup fonk calıstı.Ardından alttaki method Group.SendAsync
+        //methodu ile grouptaki clientlara mesaj gonderdık.
+        //Group name lazım old. için. 
+        //public async Task SendMessageAsync(string message, string groupName)
+        //{
+        //    await Clients.Group(groupName).SendAsync("receiveMessageOnClient", message);
+        #endregion
+        #region GroupExcept
+        //Belirtilen gruptaki belirtilen clientlar dışındaki tüm clientlara bildirimde bulunur.
+
+        //Alttaki methodu kullan.Clienttada bu methoda özel fonk yazdık.
+        //public async Task SendMessageAsync(string message, string groupName,IEnumerable<string> connectionIds)
+        //{
+        //    await Clients.GroupExcept(groupName, connectionIds).SendAsync("receiveMessageOnClient", message);
+        #endregion
+        #region Groups**
+        //Birden çok gruptaki clientalra bildirimde bulunduran methoddur.
+
+        //public async Task SendMessageAsync(string message, IEnumerable<string> groupNames)
+        //{
+        //    await Clients.Groups(groupNames).SendAsync("receiveMessageOnClient", message);
+        #endregion
+        #region OthersInGroup
+        //Bildiride bulunan client haricinde grupta bulunan tüm clientlara bildirim yapan fonksiyondur.
+
+        public async Task SendMessageAsync(string message, string groupName)
+        {
+            await Clients.OthersInGroup(groupName).SendAsync("receiveMessageOnClient", message);
             #endregion
             #endregion
         }
